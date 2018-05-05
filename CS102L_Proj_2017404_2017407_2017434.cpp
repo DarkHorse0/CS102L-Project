48cefef4739595d1a0ce2575d979b86b3d8086bc
Saim <saimulhassan5@gmail.com>
2018-05-05 22:38:14 +0500

    Almost done
    Version 0.8

--- CS102L_Proj_2017404_2017407_2017434.cpp
+++ CS102L_Proj_2017404_2017407_2017434.cpp
@@ -1,52 +1,147 @@
+// CS102L Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
 #include<iostream>
 #include<fstream>
+#include<string>
+#include<iomanip>
 
 using namespace std;
 
-class Course {
+int gradeValue(char grade);
+void manager(double tRate);
+string NameSpace(string name);
+
+struct Course {
 	string name;
 	string no;
 	int creditHours;
 	char grade;
-
-public:
-	friend class Student;
 };
 
 class Student {
 	string name;
 	int studentID;
-	bool isTuitionPaid;
+	char isTuitionPaid;
 	int numberOfCourses;
+	int credits;
 	Course* courses;
+	bool memory;
 
 public:
 	Student() {}
-	Student()
-	{
-		name = n;
-		studentID = sID;
-		isTuitionPaid = iTP;
-		numberOfCourses = nOC;
-		courses = new Course[nOC];
-	}
 
 	~Student()
 	{
-		delete courses;
+		delete[] courses;
 	}
 
-	void Write()
+	void read(int ID)
 	{
-		ofstream ofile("StudentRecords.txt");
-		ofile << name << " " << studentID << " " << isTuitionPaid << " " << numberOfCourses << endl;
+		ifstream ifile("StudentRecords.txt");
+		string temp;
+		ifile >> temp >> temp;
+		memory = false;
+		while (ID != studentID && !ifile.eof())
+		{
+			ifile >> name >> studentID >> isTuitionPaid >> numberOfCourses;
+
+			name = NameSpace(name);
+
+			if (!memory)
+			{
+				courses = new Course[numberOfCourses + 1];
+				memory = true;
+			}
+
+			credits = 0;
+
+			for (int i = 0; i < numberOfCourses; i++)
+			{
+				ifile >> courses[i].name >> courses[i].no >> courses[i].creditHours >> courses[i].grade;
+				credits += courses[i].creditHours;
+			}
+		}
+		ifile.close();
+	}
+
+	double GPA()
+	{
+		int gradePoints = 0;
+		for (int i = 0; i < numberOfCourses; i++)
+		{
+			gradePoints += gradeValue(courses[i].grade) * courses[i].creditHours;
+		}
+		return gradePoints / (float)credits;
+	}
+
+	double feesAmount(double tuition)
+	{
+		return tuition * credits;
+	}
+
+	void display(double tuition)
+	{
+		cout << "Student Name: " << name << endl;
+		cout << "Student ID: " << studentID << endl;
+		cout << "Number of courses enrolled : " << numberOfCourses << endl << endl;
+
+		cout << left << setw(15) << "Course No" << setw(15) << "Course Name" << setw(11) << "Credits" << setw(9) << "Grade" << endl;
 
 		for (int i = 0; i < numberOfCourses; i++)
 		{
-			ofile << courses[i].name << " " << courses[i].no << " " << courses[i].creditHours << " " << courses[i].grade << endl;
+			cout << setw(15) << courses[i].no << setw(18) << courses[i].name << setw(10) << courses[i].creditHours << setw(9) << courses[i].grade << endl;
+		}
+
+		cout << endl << endl;
+
+		if (isTuitionPaid == 'Y')
+		{
+			cout << "Total No. of credits: " << credits << endl;
+			cout << "Mid-semester GPA: " << GPA() << endl;
+		}
+		else {
+			cout << "Due to tuition fees not being paid, result will not be shown." << endl;
+			cout << "Bill: " << feesAmount(tuition) << endl;
 		}
 	}
 };
+
+string NameSpace(string name)
+{
+	string result = "";
+	for (int i = 0; i < name.length(); i++)
+	{
+		if (i != 0 && name[i] >= 'A' && name[i] <= 'Z')
+			result += " ";
+
+		result += name[i];
+	}
+	return result;
+}
+
+int gradeValue(char grade)
+{
+	switch (grade)
+	{
+	case 'A':
+		return 4;
+		break;
+	case 'B':
+		return 3;
+		break;
+	case 'C':
+		return 2;
+		break;
+	case 'D':
+		return 1;
+		break;
+	case 'F':
+		return 0;
+		break;
+	}
+}
 
 int main()
 {
@@ -54,6 +149,28 @@ int main()
 	int noOfStudents;
 	double tuitionRate;
 	record >> noOfStudents >> tuitionRate;
-	cout << noOfStudents;
+	record.close();
+
+	manager(tuitionRate);
+
 	return 0;
+}
+
+void manager(double tRate)
+{
+	char choice = 'Y';
+	int stID;
+	Student std;
+	while (toupper(choice) == 'Y')
+	{
+		cout << endl << "Enter student ID to display record: ";
+		cin >> stID;
+		cout << endl;
+
+		std.read(stID);
+		std.display(tRate);
+		cout << endl << "Enter Y to see more records. Enter any other key to exit.";
+		cin >> choice;
+		system("CLS");
+	}
 }
