57f65c76152a40d5e54012772b1efb5c2ea6e827
Saim <saimulhassan5@gmail.com>
2018-05-05 17:27:11 +0500

    Added classes
    Not finished yet.

--- CS102L_Proj_2017404_2017407_2017434.cpp
+++ CS102L_Proj_2017404_2017407_2017434.cpp
@@ -1,6 +1,52 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
+
+class Course{
+	string name;
+	string no;
+	int creditHours;
+	char grade;
+	
+public:
+	friend class Student;
+};
+
+class Student{
+	string name;
+	int studentID;
+	bool isTuitionPaid;
+	int numberOfCourses;
+	Course* courses;
+
+public:
+	Student() {}
+	Student(string n, int sID, bool iTP, int nOC)
+	{
+		name = n;
+		studentID = sID;
+		isTuitionPaid = iTP;
+		numberOfCourses = nOC;
+		courses = new Course[nOC];
+	}
+
+	~Student()
+	{
+		delete courses;
+	}
+
+	void Write()
+	{
+		ofstream ofile("StudentRecords.txt");
+		ofile << name << " " << studentID << " " << isTuitionPaid << " " << numberOfCourses << endl;
+		
+		for (int i = 0; i < numberOfCourses; i++)
+		{
+			ofile << courses[i].name << " " << courses[i].no << " " << courses[i].creditHours << " " << courses[i].grade << endl;
+		}
+	}
+};
 
 int main()
 {
