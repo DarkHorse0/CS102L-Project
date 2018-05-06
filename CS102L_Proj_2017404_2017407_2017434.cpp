4c1f56fdcf00e2b2ce82f5ef51e4cb1ae6e319c5
Saim <saimulhassan5@gmail.com>
2018-05-06 23:04:13 +0500

    Final Version
    Finished

--- CS102L_Proj_2017404_2017407_2017434.cpp
+++ CS102L_Proj_2017404_2017407_2017434.cpp
@@ -1,7 +1,3 @@
-// CS102L Project.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -9,10 +5,8 @@
 
 using namespace std;
 
-int gradeValue(char grade);
-void manager(double tRate);
-string NameSpace(string name);
 
+//Struct Definition for Course
 struct Course {
 	string name;
 	string no;
@@ -20,52 +14,76 @@ struct Course {
 	char grade;
 };
 
+
+//Function Prototypes
+int gradeValue(char grade);
+void manager(double tRate);
+string NameSpace(string name);
+void BubbleSort(Course c[], int size);
+
+
+//Class defined for holding Student record data in program
 class Student {
+	//Class variables
 	string name;
 	int studentID;
 	char isTuitionPaid;
 	int numberOfCourses;
 	int credits;
-	Course* courses;
-	bool memory;
+	Course* courses;	// Pointer to allow dynamic memory allocation later
 
 public:
-	Student() {}
-
+	//Destructor to release allocated memory
 	~Student()
 	{
 		delete[] courses;
 	}
 
-	void read(int ID)
+	//Function to read record data from file. Takes ID as argument, keeps reading from file until it finds a record whose ID matches argument.
+	bool read(int ID)
 	{
 		ifstream ifile("StudentRecords.txt");
+		//Read first line of data which doesn't hold student record
 		string temp;
 		ifile >> temp >> temp;
-		memory = false;
-		while (ID != studentID && !ifile.eof())
+
+		//keeps reading until ID matches argument or end of file reached
+		while (!ifile.eof())
 		{
 			ifile >> name >> studentID >> isTuitionPaid >> numberOfCourses;
 
-			name = NameSpace(name);
+			name = NameSpace(name);	//Function to separate name from camelCase into string with spaces
 
-			if (!memory)
-			{
-				courses = new Course[numberOfCourses + 1];
-				memory = true;
-			}
+			courses = new Course[numberOfCourses + 1];	//Memory dynamically allocated according to number of courses to be read
 
 			credits = 0;
-
+			//loop to read course data into struct array and calculate total credits
 			for (int i = 0; i < numberOfCourses; i++)
 			{
 				ifile >> courses[i].name >> courses[i].no >> courses[i].creditHours >> courses[i].grade;
 				credits += courses[i].creditHours;
 			}
+
+			//reads lines of . separating student records
+			ifile >> temp >> temp >> temp;
+
+			if (ID == studentID)	//Close file and return true if record found
+			{
+				ifile.close();
+				return true;
+			}
+			else if (ifile.eof())	//Close file and return false if record not found and EOF reached
+			{
+				ifile.close();
+				return false;
+			}
+			else {					//Delete allocated memory so new memory of appropriate size can be allocated for the next record
+				delete[] courses;
+			}
 		}
-		ifile.close();
 	}
 
+	//Function to calculate GPA and return it
 	double GPA()
 	{
 		int gradePoints = 0;
@@ -76,43 +94,100 @@ public:
 		return gradePoints / (float)credits;
 	}
 
+	//Function to return total fees
 	double feesAmount(double tuition)
 	{
 		return tuition * credits;
 	}
 
+	//Functino to display formatted student grade report
 	void display(double tuition)
 	{
 		cout << "Student Name: " << name << endl;
 		cout << "Student ID: " << studentID << endl;
 		cout << "Number of courses enrolled : " << numberOfCourses << endl << endl;
 
-		cout << left << setw(15) << "Course No" << setw(15) << "Course Name" << setw(11) << "Credits" << setw(9) << "Grade" << endl;
-
-		for (int i = 0; i < numberOfCourses; i++)
-		{
-			cout << setw(15) << courses[i].no << setw(18) << courses[i].name << setw(10) << courses[i].creditHours << setw(9) << courses[i].grade << endl;
-		}
-
-		cout << endl << endl;
-
+		//Displays Grades if tuition paid, message with Pending Fees if not paid
 		if (isTuitionPaid == 'Y')
 		{
+			BubbleSort(courses, numberOfCourses);
+
+			cout << left << setw(15) << "Course No" << setw(15) << "Course Name" << setw(11) << "Credits" << setw(9) << "Grade" << endl;
+			for (int i = 0; i < numberOfCourses; i++)
+			{
+				cout << setw(15) << courses[i].no << setw(18) << courses[i].name << setw(10);
+				cout << courses[i].creditHours << setw(9) << courses[i].grade << endl;
+			}
+
+			cout << endl << endl;
 			cout << "Total No. of credits: " << credits << endl;
 			cout << "Mid-semester GPA: " << GPA() << endl;
 		}
 		else {
 			cout << "Due to tuition fees not being paid, result will not be shown." << endl;
-			cout << "Bill: " << feesAmount(tuition) << endl;
+			cout << "Pending Fees: " << feesAmount(tuition) << endl;
 		}
 	}
 };
 
+
+
+
+int main()
+{
+	//Records file opened to read No of Students and Tuition Rate
+	ifstream record("StudentRecords.txt");
+	int noOfStudents;
+	double tuitionRate;
+	record >> noOfStudents >> tuitionRate;
+	record.close();
+
+	//Function to manage
+	manager(tuitionRate);
+
+	return 0;
+}
+
+//Function to manage user input, creating object to hold record and displaying
+void manager(double tRate)
+{
+	char choice = 'Y';
+	int stID;
+	Student std;
+
+	//loop runs until user enters a terminating value
+	while (toupper(choice) == 'Y')
+	{
+		cout << endl << "Enter student ID to display record: ";
+		cin >> stID;
+		cout << endl;
+
+		//Function in if returns true if ID passed in argument is found in file, false if not
+		if (std.read(stID))
+		{
+			//displays formatted grade report for relevant student
+			system("CLS");
+			std.display(tRate);
+			cout << endl << "Enter Y to see more records. Enter any other key to exit.";
+			cin >> choice;
+			system("CLS");
+		}
+		else {
+			//asks user to enter different ID
+			system("CLS");
+			cout << "No record with that ID found. Please enter another ID.";
+		}
+	}
+}
+
+
+//Function to separate string in CamelCase into string with spaces  
 string NameSpace(string name)
 {
 	string result = "";
 	for (int i = 0; i < name.length(); i++)
 	{
+		//if letter is capital and not first letter then adds a space to final string
 		if (i != 0 && name[i] >= 'A' && name[i] <= 'Z')
 			result += " ";
 
@@ -121,6 +196,8 @@ string NameSpace(string name)
 	return result;
 }
 
+
+//Function that returns a numerical value for the alphabetical grade passed in argument based on given criteria
 int gradeValue(char grade)
 {
 	switch (grade)
@@ -143,34 +220,28 @@ int gradeValue(char grade)
 	}
 }
 
-int main()
+//Function to sort Course array from Student class object in order of course code using BubbleSort Algorithm
+void BubbleSort(Course c[], int size)
 {
-	ifstream record("StudentRecords.txt");
-	int noOfStudents;
-	double tuitionRate;
-	record >> noOfStudents >> tuitionRate;
-	record.close();
+	bool sort = true;
 
-	manager(tuitionRate);
-
-	return 0;
-}
-
-void manager(double tRate)
-{
-	char choice = 'Y';
-	int stID;
-	Student std;
-	while (toupper(choice) == 'Y')
+	//outer while loop runs until no sorting operation takes place in inner for loop
+	while (sort)
 	{
-		cout << endl << "Enter student ID to display record: ";
-		cin >> stID;
-		cout << endl;
-
-		std.read(stID);
-		std.display(tRate);
-		cout << endl << "Enter Y to see more records. Enter any other key to exit.";
-		cin >> choice;
-		system("CLS");
+		sort = false;
+		//for loop runs over array's indexes except last
+		for (int i = 0; i < size - 1; i++)
+		{
+			//consecutive elements of array are compared and swapped if earlier ones are larger than later ones
+			if (c[i].no > c[i + 1].no)
+			{
+				sort = true;
+				//Swapping elements
+				Course temp = c[i];
+				c[i] = c[i + 1];
+				c[i + 1] = temp;
+			}
+		}
 	}
+
 }
